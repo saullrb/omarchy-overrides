f2c82f91ab2eeabff0bd5711f1cb0f752f540274
Saull Brandao <saull@outlook.com>
2025-07-19 13:01:00 -0300

    add starship

--- migrations/1752940226.sh
+++ migrations/1752940226.sh
@@ -1,9 +1,6 @@
 if ! command -v starship &>/dev/null; then
-  echo "Add starship"
+  echo "Installing starship"
   yay -S --noconfirm --needed starship
-
-  grep -qxF 'eval "$(starship init bash)"' ~/.bashrc ||
-    echo 'eval "$(starship init bash)"' >>~/.bashrc
 
   exec bash
 fi
