577d2cb237c018ff7f4d2a4bb032ba07efc417ac
Saull Brandao <saull@outlook.com>
2025-07-19 17:42:09 -0300

    reorganize fish config and add bindings

---
+++ config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,22 @@
+# Git
+abbr -a g git
+abbr -a ga 'git add'
+abbr -a gaa 'git add --all'
+abbr -a gcl 'git clone'
+abbr -a gc 'git commit'
+abbr -a gcm 'git commit -m'
+abbr -a gcam 'git commit -a -m'
+abbr -a gd 'git diff'
+abbr -a gf 'git fetch'
+abbr -a gp 'git push'
+abbr -a gl 'git pull'
+abbr -a gst 'git status'
+abbr -a gco 'git checkout'
+abbr -a gcb 'git checkout -b'
+abbr -a glog 'git log --oneline --decorate'
+abbr -a glogy 'git log --since='yesterday.midnight' --until='midnight' --all --no-merges --oneline --author=saull@outlook.com'
+
+# Tools
+abbr -a n nvim
+abbr -a ou omarchy-update
+abbr -a oo omarchy-override
--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -1,21 +1,3 @@
-# Git
-abbr -a g git
-abbr -a ga 'git add'
-abbr -a gaa 'git add --all'
-abbr -a gcl 'git clone'
-abbr -a gc 'git commit'
-abbr -a gcm 'git commit -m'
-abbr -a gcam 'git commit -a -m'
-abbr -a gd 'git diff'
-abbr -a gf 'git fetch'
-abbr -a gp 'git push'
-abbr -a gl 'git pull'
-abbr -a gst 'git status'
-abbr -a gco 'git checkout'
-abbr -a gcb 'git checkout -b'
-abbr -a glog 'git log --oneline --decorate'
-abbr -a glogy 'git log --since='yesterday.midnight' --until='midnight' --all --no-merges --oneline --author=saull@outlook.com'
-
 # Zoxide
 function zd
     if test (count $argv) -eq 0
@@ -34,9 +16,6 @@ alias lsa='ls -a'
 alias lt='eza --tree --level=2 --long --icons --git'
 alias lta='lt -a'
 alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
-
-# Tools
-alias n='nvim'
 
 # Find packages without leaving the terminal
 alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
