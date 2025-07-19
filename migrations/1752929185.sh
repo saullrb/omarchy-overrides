312a732e6c69f63d01235400b128b40fe112a5b7
Saull Brandao <saull@outlook.com>
2025-07-19 11:04:04 -0300

    remove/replace some apps, change a few keybinds and add smart gaps

---
+++ migrations/1752929185.sh
@@ -0,0 +1,45 @@
+echo "Removing/Replacing some apps"
+source ~/.local/share/omarchy/default/bash/functions
+
+web2app-remove "Basecamp" 2>/dev/null
+web2app-remove "GitHub" 2>/dev/null
+web2app-remove "Google Contacts" 2>/dev/null
+web2app-remove "Google Messages" 2>/dev/null
+web2app-remove "Google Photos" 2>/dev/null
+web2app-remove "HEY" 2>/dev/null
+web2app-remove "X" 2>/dev/null
+web2app-remove "YouTube" 2>/dev/null
+
+if command -v spotify &>/dev/null; then
+  echo "Replacing Spotify with Youtube Music webapp..."
+  yay -Rns --noconfirm spotify
+  web2app "YouTube Music" https://music.youtube.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube-music.png
+fi
+
+if command -v xournalpp &>/dev/null; then
+  echo "Removing Xournal++..."
+  yay -Rns --noconfirm xournalpp
+fi
+
+if command -v typora &>/dev/null; then
+  echo "Removing Typora..."
+  yay -Rns --noconfirm typora
+fi
+
+if command -v pinta &>/dev/null; then
+  echo "Replacing Pinta with Gimp..."
+  yay -Rns --noconfirm pinta
+  yay -S --noconfirm --needed gimp
+fi
+
+if command -v signal-desktop &>/dev/null; then
+  echo "Removing Signal..."
+  yay -Rns --noconfirm signal-desktop
+fi
+
+if command -v obsidian &>/dev/null; then
+  echo "Removing Obsidian..."
+  yay -Rns --noconfirm obsidian-bin
+fi
+
+cp ~/.local/share/omarchy-overrides/config/hypr/custom.conf ~/.config/hypr/
