e44012b8cc57a1b027033c13c3ae077b19782e0d
Saull Brandao <saull@outlook.com>
2025-07-19 16:38:29 -0300

    remove the copy part from the zellij migration

--- migrations/1752943876.sh
+++ migrations/1752943876.sh
@@ -2,6 +2,5 @@ if ! command -v zellij &>/dev/null; then
   echo "Installing zellij"
   yay -S --noconfirm --needed zellij
 
-  cp ~/.local/share/omarchy-overrides/config/zellij/config.kdl ~/.config/zellij/
   exec bash
 fi
