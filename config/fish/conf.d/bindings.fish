644bd71c7019c054546e7e447ffc3b0b8071b69c
Saull Brandão <saullrb@proton.me>
2025-07-28 07:46:16 -0300

    go back to using bash

--- config/fish/conf.d/bindings.fish
+++
@@ -1,1 +0,0 @@
-bind \cf zellij-sessionizer
--- config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    source ~/.config/fish/functions/functions.fish
-
-    zoxide init fish | source
-    starship init fish | source
-end
