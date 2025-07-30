9bf87f63a3e360ee3f371cb535ba34af33db6828
Saull Brandão <saullrb@proton.me>
2025-07-30 19:11:19 -0300

    format all files

--- boot.sh
+++ boot.sh
@@ -6,29 +6,29 @@ REPO_DIR="$HOME/.local/share/omarchy-overrides"
 MIGRATIONS_DIR="$REPO_DIR/migrations"
 
 if [ -d "$REPO_DIR/.git" ]; then
-  echo "Updating System Setup..."
-  git -C "$REPO_DIR" pull --quiet
+    echo "Updating System Setup..."
+    git -C "$REPO_DIR" pull --quiet
 else
-  echo "Cloning System Setup..."
-  git clone "$REPO_URL" "$REPO_DIR" >/dev/null
+    echo "Cloning System Setup..."
+    git clone "$REPO_URL" "$REPO_DIR" >/dev/null
 fi
 
 # Run all migrations
 for migration in "$MIGRATIONS_DIR"/*.sh; do
-  if [ -f "$migration" ]; then
-    echo "Running migration $migration..."
-    bash "$migration"
-  fi
+    if [ -f "$migration" ]; then
+        echo "Running migration $migration..."
+        bash "$migration"
+    fi
 done
 
 # Add custom hypr config
 HYPR_CUSTOM="source = $REPO_DIR/default/hypr/custom.conf"
 grep -qxF "$HYPR_CUSTOM" ~/.config/hypr/hyprland.conf ||
-  echo "$HYPR_CUSTOM" >>~/.config/hypr/hyprland.conf
+    echo "$HYPR_CUSTOM" >>~/.config/hypr/hyprland.conf
 
 # Add custom bash config
 BASH_CUSTOM="source $REPO_DIR/default/bash/custom"
 grep -qxF "$BASH_CUSTOM" ~/.bashrc ||
-  echo "$BASH_CUSTOM" >>~/.bashrc
+    echo "$BASH_CUSTOM" >>~/.bashrc
 
 echo "All done!"
--- migrations/terminal.sh
+++ migrations/terminal.sh
@@ -1,21 +1,21 @@
 if ! command -v starship &>/dev/null; then
-  echo "Installing starship"
-  yay -S --noconfirm --needed starship
+    echo "Installing starship"
+    yay -S --noconfirm --needed starship
 fi
 
 if ! command -v tmux &>/dev/null; then
-  echo "Installing tmux"
-  yay -S --noconfirm --needed tmux
+    echo "Installing tmux"
+    yay -S --noconfirm --needed tmux
 fi
 
 # Add my custom nvim plugins
 if [ ! -f ~/.config/nvim/lua/plugins/custom.lua ]; then
-  cat >~/.config/nvim/lua/plugins/custom.lua <<'EOF'
+    cat >~/.config/nvim/lua/plugins/custom.lua <<'EOF'
 return dofile(vim.fn.expand("~/.local/share/omarchy-overrides/default/nvim/plugins.lua")) or {}
 EOF
 fi
 
 # Add my custom nvim config
 if ! grep -Fq 'dofile(vim.fn.expand("~/.local/share/omarchy-overrides/default/nvim/config.lua"))' ~/.config/nvim/lua/config/options.lua; then
-  echo 'dofile(vim.fn.expand("~/.local/share/omarchy-overrides/default/nvim/config.lua"))' >>~/.config/nvim/lua/config/options.lua
+    echo 'dofile(vim.fn.expand("~/.local/share/omarchy-overrides/default/nvim/config.lua"))' >>~/.config/nvim/lua/config/options.lua
 fi
