1305569a8e7f80c9adfae78fdccd6d35348a757f
Saull Brandão <saullrb@proton.me>
2025-08-25 11:46:07 -0300

    change to curl

--- migrations/config.sh
+++ migrations/config.sh
@@ -24,5 +24,5 @@ grep -qxF 'source ~/.local/share/omarchy-overrides/default/bash/custom' ~/.bashr
 if [ ! -f "$HOME/.config/mpv/scripts/autoload.lua" ]; then
   echo "Adding mpv autoload script"
   mkdir -p ~/.config/mpv/scripts
-  wget -O ~/.config/mpv/scripts/autoload.lua https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/autoload.lua
+  curl -Lo ~/.config/mpv/scripts/autoload.lua https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/autoload.lua
 fi
