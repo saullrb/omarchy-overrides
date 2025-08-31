9c1dc3e8bc7f5ea592821427526a0f4f6d2a2263
Saull Brandão <saullrb@proton.me>
2025-08-31 17:07:57 -0300

    remove relative line numbers from nvim and zed

--- default/nvim/config.lua
+++ default/nvim/config.lua
@@ -3,17 +3,11 @@ vim.opt.wrap = true
 vim.opt.showbreak = "↪"
 vim.opt.breakindent = true
 
-vim.opt.relativenumber = true
-
 -- Disable the system clipboard integration
 vim.opt.clipboard = ""
 
 -- Disable all snacks animations
 vim.g.snacks_animate = false
-
--- Easy insertion of a trailing ; or , from insert mode.
-vim.keymap.set("i", ";;", "<Esc>A;<Esc>")
-vim.keymap.set("i", ",,", "<Esc>A,<Esc>")
 
 -- Yank to the system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
