919252eac8a0b8a32e99f1a060ea2e8239a2243c
Saull Brandão <saullrb@proton.me>
2025-08-31 17:18:26 -0300

    make nvim yank/paste/delete work like zed

--- default/nvim/config.lua
+++ default/nvim/config.lua
@@ -3,11 +3,30 @@ vim.opt.wrap = true
 vim.opt.showbreak = "↪"
 vim.opt.breakindent = true
 
--- Disable the system clipboard integration
-vim.opt.clipboard = ""
-
 -- Disable all snacks animations
 vim.g.snacks_animate = false
 
--- Yank to the system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+-- Disable global clipboard
+vim.opt.clipboard = ""
+
+-- always yank to system clipboard
+local map = vim.keymap.set
+map({ "n", "v" }, "y", '"+y')
+map("n", "Y", '"+Y')
+
+-- paste: if last op was yank, use system clipboard; else use default
+map("n", "p", function()
+	return vim.fn.reg_recording() == ""
+			and vim.fn.getregtype("+") == vim.fn.getregtype('"')
+			and vim.fn.getreg('"') == vim.fn.getreg("+")
+			and '"+p'
+		or "p"
+end, { expr = true })
+
+map("n", "P", function()
+	return vim.fn.reg_recording() == ""
+			and vim.fn.getregtype("+") == vim.fn.getregtype('"')
+			and vim.fn.getreg('"') == vim.fn.getreg("+")
+			and '"+P'
+		or "P"
+end, { expr = true })
